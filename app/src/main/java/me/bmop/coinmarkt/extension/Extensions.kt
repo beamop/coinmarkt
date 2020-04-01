d505b510becc11a8b54bece6e85ccf0777988ea3
beamop <beamop@pm.me>
2020-04-01 15:00:40 +0200

    Add Exception package with Extensions.kt to not have extensions dispersed all around the code

---
+++ app/src/main/java/me/bmop/coinmarkt/extension/Extensions.kt
@@ -0,0 +1,8 @@
+import androidx.recyclerview.widget.RecyclerView
+
+fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+    itemView.setOnClickListener {
+        event.invoke(adapterPosition, itemViewType)
+    }
+    return this
+}
